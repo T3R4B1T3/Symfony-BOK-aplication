c6da5649ccd57153f99a5f411dedfbdf8b1be5a9
Niepelnosprytny <sebastiankonkol2206@gmail.com>
2022-09-02 12:40:17 +0200

    initial form validation

--- migrations/Version20220901113558.php
+++ migrations/Version20220901113558.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220901083229 extends AbstractMigration
+final class Version20220901113558 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,7 +22,7 @@ final class Version20220901083229 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE report (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, shop_id INT NOT NULL, report_log_id INT NOT NULL, description LONGTEXT NOT NULL, email VARCHAR(255) DEFAULT NULL, phone_number VARCHAR(9) DEFAULT NULL, report_date DATE NOT NULL, user_agent VARCHAR(255) NOT NULL, INDEX IDX_C42F778412469DE2 (category_id), INDEX IDX_C42F77844D16C4DD (shop_id), UNIQUE INDEX UNIQ_C42F7784C3948819 (report_log_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE report_log (id INT AUTO_INCREMENT NOT NULL, `read` TINYINT(1) NOT NULL, first_who_read VARCHAR(255) DEFAULT NULL, read_date DATE DEFAULT NULL, state VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE report_log (id INT AUTO_INCREMENT NOT NULL, seen TINYINT(1) NOT NULL, first_who_read VARCHAR(255) DEFAULT NULL, read_date DATE DEFAULT NULL, state VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE shop (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
--- src/Form/ReportType.php
+++ src/Form/ReportType.php
@@ -5,28 +5,47 @@ namespace App\Form;
 use App\Entity\Category;
 use App\Entity\Report;
 use App\Entity\Shop;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Regex;
 
-class ReportType extends AbstractType
+class ReportType extends AbstractType implements FormTypeInterface
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('description')
-            ->add('email')
-            ->add('phone_number')
-            ->add('category',  EntityType::class, [
+            ->add('description', TextType::class, [
+                'required' => true,
+                'constraints' => [new Length(['min' => 30]),
+                    new Regex(['pattern' => "^[\w',]+\s[\w',]+\s[\w',]+^",
+                    ])],
+            ])
+            ->add('email', EmailType::class, [
+                'required' => false,
+                'constraints' => [new Email([
+                    'mode' => 'strict'
+                ])],
+            ])
+            ->add('phone_number', IntegerType::class, [
+                'required' => false,
+                'constraints' => [new Length(['min' => 9, 'max' => 9])]
+            ])
+            ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
             ])
-            ->add('shop',EntityType::class, [
+            ->add('shop', EntityType::class, [
                 'class' => Shop::class,
                 'choice_label' => 'name',
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
