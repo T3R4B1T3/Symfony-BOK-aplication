de3c97fa5ef8c7dd76beea7e1f860722bac9ad1e
Niepelnosprytny <sebastiankonkol2206@gmail.com>
2022-09-20 10:00:03 +0200

    fixes to comments form

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -3,8 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Comment;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +13,13 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Content')
-            ->add('SUBMIT', SubmitType::class);
+            ->add('Content', TextType::class, [
+                'required' => true,
+                'label' => "POST COMMENT",
+                'attr'=>array(
+                    'placeholder' => 'Type your comment here'
+                ),
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
