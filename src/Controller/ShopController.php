130f37ef64d4a0f351158a25774690d80607a37c
Michal <michalkopczyk@outlook.com>
2022-09-21 10:54:18 +0200

    Aded shop fucntionality

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -2,17 +2,62 @@
 
 namespace App\Controller;
 
+use App\Entity\Shop;
+use App\Form\ShopType;
+use App\Repository\ShopRepository;
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Request;
 class ShopController extends AbstractController
 {
-    #[Route('/shop', name: 'app_shop')]
-    public function index(): Response
+    #[Route('/shop', name: 'app_shop',methods: ['GET'])]
+    public function index(ShopRepository $shopRepository): Response
     {
         return $this->render('shop/index.html.twig', [
-            'controller_name' => 'ShopController',
+            'shops' => $shopRepository->findAll(),
         ]);
+    }
+    #[Route('/shop/new', name: 'app_shop_new', methods: ['GET','POST'])]
+    public function show(ShopRepository $shopRepository,Request $request): Response
+    {
+        $shop = new Shop();
+        $form = $this->createForm(ShopType::class, $shop);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $shopRepository->add($shop,true);
+            return $this->redirectToRoute('app_shop', [], Response::HTTP_SEE_OTHER);
+        }
+        return $this->renderForm('shop/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+    #[Route('/shop/{id}/edit', name: 'app_shop_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request,ShopRepository $shopRepository,Shop $shop): Response
+    {
+
+        $form = $this->createForm(ShopType::class, $shop);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $shopRepository->add($shop, true);
+            return $this->redirectToRoute('app_shop', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('shop/edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_shop_delete', methods: ['POST'])]
+    public function delete(Request $request, Shop $shop, ShopRepository $shopRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $shop->getId(), $request->request->get('_token'))) {
+            $shopRepository->remove($shop, true);
+        }
+
+        return $this->redirectToRoute('app_shop', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/ShopType.php
+++ src/Form/ShopType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Shop;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +13,32 @@ class ShopType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('City')
-            ->add('PostCode')
-            ->add('PhoneNumber')
-            ->add('Region')
+            ->add('name',TextType::class, [
+                'label'=>'Street and Number',
+                'attr'=>array(
+                    'placeholder'=>'e.g. Sczecińska 58'
+                )
+        ])
+            ->add('City',TextType::class,[
+                'attr'=>array(
+                    'placeholder'=>'e.g. Słupsk'
+                )
+            ])
+            ->add('PostCode',TextType::class,[
+                'attr'=>array(
+                    'placeholder'=>'e.g. 76-200'
+                )
+            ])
+            ->add('PhoneNumber',TextType::class,[
+                'attr'=>array(
+                    'placeholder'=>'e.g. 444-888-777'
+                )
+            ])
+            ->add('Region',TextType::class,[
+                'attr'=>array(
+                    'placeholder'=>'e.g. Pomorskie'
+                )
+            ])
         ;
     }
 
