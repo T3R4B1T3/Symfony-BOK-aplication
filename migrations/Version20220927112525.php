d69077b69fbe7772c70033adf79eccf140511acf
Niepelnosprytny <sebastiankonkol2206@gmail.com>
2022-09-27 13:26:29 +0200

    added UserFixtures

--- migrations/Version20220927112525.php
+++ migrations/Version20220927112525.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220920094909 extends AbstractMigration
+final class Version20220927112525 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -24,9 +24,11 @@ final class Version20220920094909 extends AbstractMigration
         $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, report_id INT NOT NULL, content LONGTEXT NOT NULL, date DATE NOT NULL, INDEX IDX_9474526CA76ED395 (user_id), INDEX IDX_9474526C4BD2A4C0 (report_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE report (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, shop_id INT NOT NULL, report_log_id INT NOT NULL, description LONGTEXT NOT NULL, email VARCHAR(255) DEFAULT NULL, phone_number VARCHAR(11) DEFAULT NULL, report_date DATE NOT NULL, user_agent VARCHAR(255) NOT NULL, user_agreement TINYINT(1) NOT NULL, INDEX IDX_C42F778412469DE2 (category_id), INDEX IDX_C42F77844D16C4DD (shop_id), UNIQUE INDEX UNIQ_C42F7784C3948819 (report_log_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE report_log (id INT AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, seen TINYINT(1) NOT NULL, first_who_read VARCHAR(255) DEFAULT NULL, read_date DATE DEFAULT NULL, INDEX IDX_7FC410425D83CC1 (state_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE shop (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, city VARCHAR(255) DEFAULT NULL, post_code VARCHAR(255) DEFAULT NULL, phone_number VARCHAR(11) DEFAULT NULL, region VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE state (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_role (user_id INT NOT NULL, role_id INT NOT NULL, INDEX IDX_2DE8C6A3A76ED395 (user_id), INDEX IDX_2DE8C6A3D60322AC (role_id), PRIMARY KEY(user_id, role_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C4BD2A4C0 FOREIGN KEY (report_id) REFERENCES report (id)');
@@ -34,6 +36,8 @@ final class Version20220920094909 extends AbstractMigration
         $this->addSql('ALTER TABLE report ADD CONSTRAINT FK_C42F77844D16C4DD FOREIGN KEY (shop_id) REFERENCES shop (id)');
         $this->addSql('ALTER TABLE report ADD CONSTRAINT FK_C42F7784C3948819 FOREIGN KEY (report_log_id) REFERENCES report_log (id)');
         $this->addSql('ALTER TABLE report_log ADD CONSTRAINT FK_7FC410425D83CC1 FOREIGN KEY (state_id) REFERENCES state (id)');
+        $this->addSql('ALTER TABLE user_role ADD CONSTRAINT FK_2DE8C6A3A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE user_role ADD CONSTRAINT FK_2DE8C6A3D60322AC FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
@@ -45,13 +49,17 @@ final class Version20220920094909 extends AbstractMigration
         $this->addSql('ALTER TABLE report DROP FOREIGN KEY FK_C42F77844D16C4DD');
         $this->addSql('ALTER TABLE report DROP FOREIGN KEY FK_C42F7784C3948819');
         $this->addSql('ALTER TABLE report_log DROP FOREIGN KEY FK_7FC410425D83CC1');
+        $this->addSql('ALTER TABLE user_role DROP FOREIGN KEY FK_2DE8C6A3A76ED395');
+        $this->addSql('ALTER TABLE user_role DROP FOREIGN KEY FK_2DE8C6A3D60322AC');
         $this->addSql('DROP TABLE category');
         $this->addSql('DROP TABLE comment');
         $this->addSql('DROP TABLE report');
         $this->addSql('DROP TABLE report_log');
+        $this->addSql('DROP TABLE role');
         $this->addSql('DROP TABLE shop');
         $this->addSql('DROP TABLE state');
         $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE user_role');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
--- src/Controller/RoleController.php
+++ src/Controller/RoleController.php
@@ -12,30 +12,32 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RoleController extends AbstractController
 {
-    #[Route('/role', name: 'app_role',methods: ['GET'])]
+    #[Route('/role', name: 'app_role', methods: ['GET'])]
     public function index(RoleRepository $roleRepository): Response
     {
         return $this->render('role/index.html.twig', [
             'roles' => $roleRepository->findAll(),
         ]);
     }
-    #[Route('/role/new', name: 'app_role_new', methods: ['GET','POST'])]
-    public function show(RoleRepository $roleRepository,Request $request): Response
+
+    #[Route('/role/new', name: 'app_role_new', methods: ['GET', 'POST'])]
+    public function show(RoleRepository $roleRepository, Request $request): Response
     {
         $role = new Role();
         $form = $this->createForm(RoleType::class, $role);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
-            $roleRepository->add($role,true);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $roleRepository->add($role, true);
             return $this->redirectToRoute('app_role', [], Response::HTTP_SEE_OTHER);
         }
         return $this->renderForm('role/new.html.twig', [
             'form' => $form,
         ]);
     }
+
     #[Route('/role/{id}/edit', name: 'app_role_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request,RoleRepository $roleRepository,Role $role): Response
+    public function edit(Request $request, RoleRepository $roleRepository, Role $role): Response
     {
 
         $form = $this->createForm(RoleType::class, $role);
@@ -50,6 +52,7 @@ class RoleController extends AbstractController
             'form' => $form,
         ]);
     }
+
     #[Route('/{id}', name: 'app_role_delete', methods: ['POST'])]
     public function delete(Request $request, Role $role, RoleRepository $roleRepository): Response
     {
